384be3542eadb54619d6522123514d7dbd778e88
Gibran Hemani <explodecomputer@gmail.com>
2019-10-27 21:26:47 +0000

    liftover

--- resources/gwas2vcf.sh
+++ resources/gwas2vcf.sh
@@ -12,5 +12,15 @@ cd gwas2vcf
 source ./venv/bin/activate
 ./venv/bin/python main.py \
 	--json ${gwasdir}/${id}/${id}_data.json \
-	--ref /mnt/storage/private/mrcieu/research/scratch/IGD/data/dev/QC/genomes/b37/human_g1k_v37.fasta
+	--ref /mnt/storage/private/mrcieu/research/scratch/IGD/data/dev/QC/genomes/hg38/hg38.fa
 
+java -jar ~/bin/picard.jar LiftoverVcf I=${gwasdir}/${id}/${id}_data.vcf.gz O=${gwasdir}/${id}/${id}_data_hg19.vcf.gz CHAIN=/mnt/storage/private/mrcieu/research/scratch/IGD/data/dev/QC/genomes/hg38/hg38ToHg19.over.chain REJECT=rejected_variants.vcf R=/mnt/storage/private/mrcieu/research/scratch/IGD/data/dev/QC/genomes/b37/human_g1k_v37.fasta WARN_ON_MISSING_CONTIG=true
+
+mv ${gwasdir}/${id}/${id}_data.vcf.gz ${gwasdir}/${id}/${id}_data_hg38.vcf.gz
+
+mv ${gwasdir}/${id}/${id}_data_hg19.vcf.gz ${gwasdir}/${id}/${id}_data.vcf.gz
+
+bcftools index -t ${gwasdir}/${id}/${id}_data.vcf.gz
+bcftools index -t ${gwasdir}/${id}/${id}_data_hg38.vcf.gz
+
+rm ${gwasdir}/${id}/${id}_data_hg19.vcf.gz.tbi

bd1d7803f700a8a2471cf459aa486a8658423ac9
Gibran Hemani <explodecomputer@gmail.com>
2019-10-28 21:57:00 +0000

    updated

--- resources/setup_directories.r
+++ resources/setup_directories.r
@@ -1,19 +1,21 @@
 library(jsonlite)
 library(dplyr)
+library(parallel)
 
 args <- commandArgs(T)
 json_template <- args[1]
-
+cores <- as.numeric(args[2])
 
 b <- read.csv(json_template, stringsAsFactors=FALSE)
 b <- subset(b, select=-c(X))
 stopifnot(all(file.exists(b$inpath)))
 b$delimiter[b$delimiter == "tab"] <- "\t"
 b$delimiter[b$delimiter == "space"] <- " "
+table(b$delimiter)
+# b <- subset(b, meta_upload == "Success")
 
-b <- subset(b, meta_upload == "Success")
 
-for(i in 1:nrow(b))
+out <- mclapply(1:nrow(b), function(i)
 {
 	message(i, " of ", nrow(b))
 	dir.create(path = dirname(b$data[i]), recursive=TRUE)
@@ -21,5 +23,5 @@ for(i in 1:nrow(b))
 	dat <- b[i, !names(b) %in% c("dirname", "jsonout", "inpath")] %>% as.list()
 	dat <- Filter(Negate(anyNA), dat)
 	write_json(dat, b$jsonout[i], auto_unbox = TRUE)
-}
+}, mc.cores=cores)
 
